91036dea5c5cd4825ef0bb3ae451336e161eb53e
Penguatix <88264676+Penguatix@users.noreply.github.com>
2022-11-17 20:33:43 +0530

    Add files via upload

---
+++ events/discordjs/messageCreate.js
@@ -0,0 +1,20 @@
+const path = require("path")
+module.exports = {
+  name: "messageCreate",
+  async execute(message){
+    const client = message.client
+    var prefix = customprefix ? customprefix : client.config.prefix
+    if (message.author.bot || !message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+    if (!cmd) return;
+    try {
+      cmd.run(client, message, args)
+    } catch (e) {
+      console.error(e)
+      message.channel.send(`An Error Occured. Please try again later`)
+  }
+  },
+}
