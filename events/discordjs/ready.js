c50d9fa0e73d048c3d60998cfff3da5dd24c8bd7
Penguatix <88264676+Penguatix@users.noreply.github.com>
2022-11-19 15:51:01 +0530

    Update ready.js

--- events/discordjs/ready.js
+++ events/discordjs/ready.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "ready",
   async execute(client){
-    client.user.setActivity("On Project Alpha", {
-      type: "WORKING",
+    client.user.setActivity("Project Alpha", {
+      type: "PLAYING",
     }),
 	  console.log('------Commands Loaded------ \n\nReady! \n' + `Logged in as ${client.user.username}`);
   },
